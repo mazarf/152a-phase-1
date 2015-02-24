6596440d3759ed7baa72aa95a3bc23f4f806f82b
Mazar Farran <mifarran@ucdavis.edu>
2015-02-23 17:09:41 -0800

    done

--- queuesim.cpp
+++ queuesim.cpp
@@ -9,6 +9,7 @@ using namespace std;
 // Global Vars
 
 double timeElapsed = 0;
+double utilizationTime = 0;
 double packetsDropped = 0; // count of all lost packets
 double area = 0; // area under the curve, used to get average queue lengths
 
@@ -24,7 +25,9 @@ typedef enum { ARRIVAL, DEPARTURE } EventType;
 double rexp(double rate) {
 	double u;
 	u = drand48();
-	return ((-1/rate)*log(1-u));
+	// stupid hacky way of handling switching between exponential
+	// and pareto distributions.
+    return ((-1/rate)*log(1-u));
 } // rexp
 
 /*
@@ -134,25 +137,35 @@ void processArrivalEvent(Event* evt, EventList& gel, queue<Event*>& buffer,
                          double lambda, double mu, int MAXBUFFER) {
     double prevTime = timeElapsed; // used to calculate stats
     timeElapsed = evt->eventTime; // update time
+    area += (timeElapsed - prevTime) * (buffer.size() + isProcessing); // might need a +1
     gel.insertEvent(new Event(ARRIVAL, lambda)); // new event
+
+    if(isProcessing) {
+            utilizationTime += timeElapsed - prevTime;
+    } // if still processing
 
     if(buffer.size() == 0 && !isProcessing) { // handle immediately
         gel.insertEvent(new Event(DEPARTURE, mu));
         isProcessing = true;
         delete evt; // maybe?
-    } else if(buffer.size() < MAXBUFFER) {
+    } else if((int)buffer.size() < MAXBUFFER) {
         buffer.push(evt); // delete event later
-        area += (timeElapsed - prevTime) * buffer.size(); // might need a +1
     } else { // buffer full, drop packet
         packetsDropped++;
         delete evt;
     }
+
+
+    //cout << "A " << timeElapsed << ' ' << prevTime << ' ' << buffer.size() << ' ' << area << endl;
+
 } // processArrivalEvent
 
 void processServiceCompletion(Event* evt, EventList& gel, queue<Event*>& buffer,
                               double lambda, double mu, int MAXBUFFER) {
     double prevTime = timeElapsed; // used to calculate stats
     timeElapsed = evt->eventTime;
+    area += (timeElapsed - prevTime) * (buffer.size()+isProcessing); // might need a +1
+    delete evt;
     // update stats
     if(buffer.size() == 0) {
         isProcessing = false;
@@ -162,16 +175,21 @@ void processServiceCompletion(Event* evt, EventList& gel, queue<Event*>& buffer,
         delete nextEvt;
         gel.insertEvent(new Event(DEPARTURE, mu));
     } // if
-    area += (timeElapsed - prevTime) * buffer.size(); // might need a +1
+    // calculate stats
+    utilizationTime += timeElapsed - prevTime;
+
+    //cout << "D " << timeElapsed << ' ' << prevTime << ' ' << buffer.size() << ' ' << area << endl;
 } // processServiceCompletion
 
-void outputStatistics() {
+void outputStatistics(double l, double m) {
     cout << "Packet loss: " << packetsDropped << endl;
     cout << "Average queue size: " << area / timeElapsed << endl;
+    cout << "Mean utilization: " << utilizationTime / timeElapsed << endl;
 } // outputStatistics
 
-void queueSim(double lambda, double mu, int MAXBUFFER) { // TODO: make maxbuffer a parameter
+void queueSim(double lambda, double mu, int MAXBUFFER) {
     timeElapsed = 0;
+    utilizationTime = 0;
     packetsDropped = 0;
     area = 0;           // reset global vars. TODO: make local. may be hard b/c Event uses them
     isProcessing = false;
@@ -181,9 +199,10 @@ void queueSim(double lambda, double mu, int MAXBUFFER) { // TODO: make maxbuffer
 	Event *firstEvt = new Event(ARRIVAL, lambda);
 	gel.insertEvent(firstEvt);
 
-	for(int i = 0; i < 100000; i++) {
+	for(int i = 0; i < 1000000; i++) {
         Event *evt = gel.getNextEvent();
-        if(evt == NULL) { // will not happen later on
+        //cout << evt->eventTime << ' ';
+        if(evt == NULL) { // failsafe. should never happen
             cout << "NULL event" << endl;
             return;
         }
@@ -192,14 +211,17 @@ void queueSim(double lambda, double mu, int MAXBUFFER) { // TODO: make maxbuffer
         } else { // DEPARTURE
             processServiceCompletion(evt, gel, buffer, lambda, mu, MAXBUFFER);
         } // if-else
+        //cout << buffer.size() << endl;
 	} // main loop
 
-	outputStatistics();
+	outputStatistics(lambda, mu);
 } // queueSim
 
 /*
  * Queue modeling program
  */
 int main() {
+    queueSim(0.9, 1.0, INT_MAX);
     queueSim(0.1, 1.0, INT_MAX);
+    queueSim(1, 1.5, INT_MAX);
 } // main
